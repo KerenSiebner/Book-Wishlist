7e9bcae71315368819dc2ee4ee7037137b70d184
Keren Siebner <kerens969@gmail.com>
2023-02-28 00:10:45 +0200

    Added stars display for book rating

--- src/cmps/book-details.jsx
+++ src/cmps/book-details.jsx
@@ -1,7 +1,16 @@
 import { bookService } from "../services/book.service";
+import {SlStar} from 'react-icons/sl'
+import { StarRating } from "./start-rating";
 
 export function BookDetails({ book, bookSelectToggle, isChecked }) {
 
+  function getStars(){
+    let stars=''
+    for(let i=0; i< Math.round(book.rating);i++) {
+    stars+='<SlStar className="star"/>'
+    }
+    return stars
+  }
   return (
     <div className="book-details-container">
       <span className="book-details-header">
@@ -11,7 +20,9 @@ export function BookDetails({ book, bookSelectToggle, isChecked }) {
       <hr />
       <h2>{book.author}</h2>
       <h4 className="book-desc">{book.description}</h4>
-      <h5>Rating: {Math.round(book.rating)}</h5>
+      {/* <h5>Rating:<span dangerouslySetInnerHTML={{__html: {getStars}}}></span></h5> */}
+      {/* <h5>Rating:     <SlStar className="star"/><SlStar className="star"/><SlStar className="star"/><SlStar className="star"/><SlStar className="star"/></h5> */}
+      <div className="rating"><h5>Rating:</h5>  <StarRating/></div>
       <h5>Price: $ {book.price}</h5>
     </div>
   );
